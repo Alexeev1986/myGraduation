dd045645cb27436ca0b33f79ee70bc6cb35a3a75
Alekseev Anton Gennadievich <gonza_ant@mail.ru>
2026-05-25 17:22:37 +0300

    Added entity MenuItem and MenuItemRepository.

---
+++ src/main/java/ru/alexeev/mygraduation/restaurant/model/MenuItem.java
@@ -0,0 +1,29 @@
+package ru.alexeev.mygraduation.restaurant.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.alexeev.mygraduation.common.model.BaseEntity;
+
+@Entity
+@Table(name = "menu_item")
+@Getter
+@Setter
+@NoArgsConstructor
+public class MenuItem extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private Menu menu;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dish_id", nullable = false)
+    private Dish dish;
+
+    public MenuItem(Integer id, Menu menu, Dish dish) {
+        super(id);
+        this.menu = menu;
+        this.dish = dish;
+    }
+}
---
+++ src/main/java/ru/alexeev/mygraduation/restaurant/repository/MenuItemRepository.java
@@ -0,0 +1,8 @@
+package ru.alexeev.mygraduation.restaurant.repository;
+
+import ru.alexeev.mygraduation.common.BaseRepository;
+import ru.alexeev.mygraduation.restaurant.model.MenuItem;
+
+public interface MenuItemRepository extends BaseRepository<MenuItem> {
+
+}
