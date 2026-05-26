b2b25b445b7b65238e065549251e59aae887b802
Alekseev Anton Gennadievich <gonza_ant@mail.ru>
2026-05-26 21:37:11 +0300

    additional checks have been added

--- src/main/java/ru/alexeev/mygraduation/restaurant/service/MenuValidator.java
+++ src/main/java/ru/alexeev/mygraduation/restaurant/service/MenuValidator.java
@@ -2,11 +2,22 @@ package ru.alexeev.mygraduation.restaurant.service;
 
 import org.springframework.stereotype.Component;
 import ru.alexeev.mygraduation.common.error.DataConflictException;
+import ru.alexeev.mygraduation.common.error.IllegalRequestDataException;
 import ru.alexeev.mygraduation.restaurant.to.MenuTo;
+
+import java.time.LocalDate;
 
 @Component
 public class MenuValidator {
     public void validate(MenuTo menuTo) {
+
+        if (menuTo.getDate().isBefore(LocalDate.now())) {
+            throw new DataConflictException("cannot add menu for past date");
+        }
+
+        if (menuTo.getDishes() == null || menuTo.getDishes().isEmpty()) {
+            throw new IllegalRequestDataException("menu must contain at least one dish");
+        }
 
         long distinctCount = menuTo.getDishes().stream()
                 .map(dishTo -> dishTo.getName().toLowerCase() + "|" + dishTo.getPrice())
